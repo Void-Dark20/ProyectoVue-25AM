d0af21a504ed91e422e175e1abfc5dda64d083d6
Dark-Void20 <jafetsanguino1125@gmail.com>
2023-03-17 09:59:09 -0500

    Jhonking el sr

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup >
 defineProps({
   msg: {
     type: String,
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Login',
+      component: LoginView
     },
     {
       path: '/about',
@@ -16,6 +17,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/Dashboard',
+      name: 'Dashboard',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Dashboard.vue')
     }
   ]
 })
